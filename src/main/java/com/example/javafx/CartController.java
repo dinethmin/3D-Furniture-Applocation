d5ad2ae43ebd6aff7c02dbb49cc83847489f56aa
dinethmin <reoakio@gmail.com>
2025-05-08 21:10:24 +0530

    cart

--- src/main/java/com/example/javafx/CartController.java
+++ src/main/java/com/example/javafx/CartController.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
